5bce528e7087745d3c56041b48898a897406287d
Oswaldo Oliveira <oswaldo.dev.oliveira@gmail.com>
2024-04-03 20:44:19 -0300

    :sparkles: feat: register new route (get-event-attendees)

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,37 @@
 import fastify from 'fastify'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 import {
   serializerCompiler,
   validatorCompiler,
+  jsonSchemaTransform,
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
 import { getEvent } from './routes/get-event'
 import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { checkIn } from './routes/check-in'
+import { getEventAttendees } from './routes/get-event-attendees'
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    info: {
+      title: 'pass.in',
+      description:
+        'Especificações da API para o back-end da aplicação pass.in.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
@@ -20,6 +41,7 @@ app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
 app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('🚀 HTTP server running')
